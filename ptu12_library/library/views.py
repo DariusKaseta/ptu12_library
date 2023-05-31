710dacacb9f0b38bf1f1c35a38356b0cba432fa7
Kestutis Januskevicius <infohata@gmail.com>
2023-05-31 09:37:04 +0200

    index view, template, css

--- ptu12_library/library/views.py
+++ ptu12_library/library/views.py
@@ -1,7 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Book, Author, BookInstance, Genre
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Sveiki Studentai!')
+    # Suskaičiuokime keletą pagrindinių objektų
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Laisvos knygos (tos, kurios turi statusą 'g')
+    num_instances_available = BookInstance.objects.filter(status__exact=0).count()
+    
+    # Kiek yra autorių    
+    num_authors = Author.objects.count()
+    
+    # perduodame informaciją į šabloną žodyno pavidale:
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'library/index.html', context)
