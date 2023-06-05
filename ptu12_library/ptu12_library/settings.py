0ca4d6319cd33d14f523fb93dc2c98d224f87624
Kestutis Januskevicius <infohata@gmail.com>
2023-06-05 08:26:42 +0200

    user profile app with login template

--- ptu12_library/ptu12_library/settings.py
+++ ptu12_library/ptu12_library/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'library',
+    'user_profile',
     'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,3 +127,4 @@ MEDIA_ROOT = BASE_DIR.joinpath(MEDIA_URL)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
