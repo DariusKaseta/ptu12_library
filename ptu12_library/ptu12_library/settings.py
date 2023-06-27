96736fdc77f2438a62b200559651b19ded7afcd9
Kestutis Januskevicius <infohata@gmail.com>
2023-06-27 14:11:27 +0200

    migrated db to pgsql

--- ptu12_library/ptu12_library/settings.py
+++ ptu12_library/ptu12_library/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'ptu12_library.local', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'ptu12_library.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'postgres',
+        'NAME': 'ptu12_library',
+        'USER': 'ptu12',
+        'PASSWORD': local_settings.DATABASE_PASSWORD,
+        'PORT': 5432
     }
 }
 
